0d4f1e1fcc6d6ab7e307c27e3355bdaff025c976
gregoremm <gregoremm@gmail.com>
2021-05-01 08:03:18 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// calling the dependency installed
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
---
+++ names.js
@@ -0,0 +1,7 @@
+// local
+const secret = "super secret";
+// share
+const peter = "Peter";
+const john = "John";
+
+module.exports = { peter, secret };
